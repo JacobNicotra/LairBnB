8385e8bb44090d65e0d81bcce1b3ba7c5a2b7120
JacobNicotra <nicotra1008@gmail.com>
2021-12-03 18:45:34 -0600

    phase 1:

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,4 +7,12 @@ router.get('/hello/world', function(req, res) {
   res.send('Hello World!');
 });
 
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
+
 module.exports = router;
+
+
+//jwyJbkfy-3QGOTAOzV_alDcDxzNaHxkYL8fA
