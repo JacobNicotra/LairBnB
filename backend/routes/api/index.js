b5aac88843734d0b888cc6d9ce9d3ea625bb6fbd
JacobNicotra <nicotra1008@gmail.com>
2021-12-05 16:10:55 -0600

    sessionReducer-complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -7,9 +7,9 @@ router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 
-router.post('/test', (req, res) => {
-  res.json({ requestBody: req.body });
-});
+// router.post('/test', (req, res) => {
+//   res.json({ requestBody: req.body });
+// });
  
 // -=-=-=-=-=-=-=-=- testing the user auth middleware routes: -=-=-=-=-=-=-=-=-
 // // GET /api/set-token-cookie
