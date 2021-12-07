0b874c8fe4d1b9db48969cab34fcc73802f0871b
JacobNicotra <nicotra1008@gmail.com>
2021-12-07 12:06:18 -0600

    get spots api

---
+++ backend/routes/api/spots.js
@@ -0,0 +1,37 @@
+// backend/routes/api/users.js
+const express = require('express')
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { Spot } = require('../../db/models');
+
+// ...
+// const { check } = require('express-validator');
+// const { handleValidationErrors } = require('../../utils/validation');
+// ...
+
+const router = express.Router();
+
+router.get('/', asyncHandler(async function(_req, res) {
+  const spots = await spots.findAll();
+  console.log('the spots: ', spots)
+  return res.json(spots);
+}));
+
+
+
+// Sign up
+// router.post(
+//   '/',
+//   asyncHandler(async (req, res) => {
+//     const { email, password, username } = req.body;
+//     const user = await User.signup({ email, username, password });
+
+//     await setTokenCookie(res, user);
+
+//     return res.json({
+//       user,
+//     });
+//   }),
+// );
+
+module.exports = router;
